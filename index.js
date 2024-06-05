ae54ff90471c5b8cf648f6572ebb0c603211a766
ben mccarthy <bmccarthy90@gmail.com>
2024-06-05 16:26:54 -0400

    Passing Objects Lab Tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name : "Ben",
+    streetAddress : "500 Washington Ave",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+   obj[key] = value;
+   return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+   const newObj = {...obj};
+   delete newObj[key];
+   return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
+// const newEmployee = destructivelyDeleteFromEmployeeByKey(employee, "name");
+
+// console.log(newEmployee);
